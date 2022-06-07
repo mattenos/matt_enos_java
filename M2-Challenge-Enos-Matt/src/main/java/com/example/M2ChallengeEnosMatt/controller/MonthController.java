50a3d07596764a938888e99b6c9d6d331ef9586c
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-06-07 18:01:35 -0400

    complete MonthController randomMonth API

--- M2-Challenge-Enos-Matt/src/main/java/com/example/M2ChallengeEnosMatt/controller/MonthController.java
+++ M2-Challenge-Enos-Matt/src/main/java/com/example/M2ChallengeEnosMatt/controller/MonthController.java
@@ -5,55 +5,70 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping(value = "/month")
 public class MonthController {
-    @GetMapping(value="/{monthNumber}")
+
+    @GetMapping(value = "/{monthNumber}")
     @ResponseStatus(value = HttpStatus.OK)
-    public String getMonth(@PathVariable String monthNumber) {
+    public Month getMonth(@PathVariable int monthNumber) {
+
+        String name = "no name";
 
         switch (monthNumber) {
-            case "1":
-                return "January";
-            case "2":
-                return "February";
-            case "3":
-                return "March";
-            case "4":
-                return "April";
-            case "5":
-                return "May";
-            case "6":
-                return "June";
-            case "7":
-                return "July";
-            case "8":
-                return "August";
-            case "9":
-                return "September";
-            case "10":
-                return "October";
-            case "11":
-                return "November";
-            case "12":
-                return "December";
+            case 1:
+                name = "January";
+                break;
+            case 2:
+                name = "February";
+                break;
+            case 3:
+                name = "March";
+                break;
+            case 4:
+                name = "April";
+                break;
+            case 5:
+                name = "May";
+                break;
+            case 6:
+                name = "June";
+                break;
+            case 7:
+                name = "July";
+                break;
+            case 8:
+                name = "August";
+                break;
+            case 9:
+                name = "September";
+                break;
+            case 10:
+                name = "October";
+                break;
+            case 11:
+                name = "November";
+                break;
+            case 12:
+                name = "December";
+                break;
             default:
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Month Number Specified");
         }
+        return new Month(monthNumber, name);
     }
 
-//    @GetMapping(value = "/randomMonth")
-//    @ResponseStatus(value = HttpStatus.OK)
-//    public String getRandomMonth(@PathVariable String monthNumber){
-//        Month randomMonth = new Month();
-//        int randomInt = randomMonth.;
-//        int correctInt = randomInt + 1;
-//        System.out.println(correctInt);
-//
-//        for (int i = 0; i < 11; i++) {
-//            System.out.print((rand.nextInt(11) + 1) + " " );
-//        }
-//
-//    }
+    @GetMapping(value = "/randomMonth")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Month getRandomMonth() {
 
+        Random rand = new Random();
+
+        int randomInt = rand.nextInt(12) + 1;
+
+        return getMonth(randomInt);
+
+    }
 }
