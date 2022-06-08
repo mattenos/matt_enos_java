0c2a709b5eacfe840e455b22a3a73826c8e8ca3f
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-06-08 09:34:30 -0400

    wrote test for randomMonth

--- M2-Challenge-Enos-Matt/src/test/java/com/example/M2ChallengeEnosMatt/controller/MonthControllerTest.java
+++ M2-Challenge-Enos-Matt/src/test/java/com/example/M2ChallengeEnosMatt/controller/MonthControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.M2ChallengeEnosMatt.controller;
 
+import com.example.M2ChallengeEnosMatt.Month;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MonthController.class)
@@ -19,25 +21,40 @@ public class MonthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Test
     public void shouldReturnMonthWhenPassedAValidMonth() throws Exception {
-        String outputJson = "September";
+
+        Month month = new Month(9, "September");
+
+        String outputJson = mapper.writeValueAsString(month);
 
         mockMvc.perform(
-                get("/month/9")
-                        .contentType(MediaType.APPLICATION_JSON)
-        )
+                        get("/month/9")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(outputJson));    }
+                .andExpect(content().string(outputJson));
+    }
 
     @Test
     public void shouldReturn422WhenPassedAnInvalidMonth() throws Exception {
         mockMvc.perform(
-                get("/month/13")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        )
+                        get("/month/13")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
                 .andDo(print())
                 .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void shouldReturnARandomMonthWhenGivenANumberWithinBounds() throws Exception {
+        mockMvc.perform(get("/month/2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number").isNotEmpty())
+                .andExpect(jsonPath("$.name").isNotEmpty());
     }
 }
